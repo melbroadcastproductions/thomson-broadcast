7c872e659fc1fd51fd61cfbfc3c72b1d364cccb4
Melchior <103557222+melbroadcastproductions@users.noreply.github.com>
2026-03-22 11:15:02 +0400

    Add files via upload

--- assets/js/site-config.js
+++ assets/js/site-config.js
@@ -1,5 +1,5 @@
 window.THOMSON_SITE_CONFIG = {
-  whatsappNumber: '971506449336',
+  whatsappNumber: '971504185758',
   whatsappMessage: 'Hello Thomson Broadcast, I would like to discuss an OB van, studio, MCR, playout, refurbishment or cloud media project.',
   contactEmail: 'sales@thomson-broadcast.tv',
   headquarters: '',
